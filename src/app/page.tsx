c3330b2a91aae5ac4a22fb9f7c5511f7bbe4d32a
Burak <burak.yn.dev@gmail.com>
2025-02-25 23:10:31 +0300

    🚧 Create heading and skills component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { Skills } from "@/components/skills";
 
 export default function Home() {
   return (
-    <div>
-      Hello world!
+    <div className="flex flex-col items-center absolute left-0 top-0 w-full h-full overflow-y-auto">
+      <div className="flex flex-col gap-6 lg:gap-12 items-center p-8 max-w-360">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl lg:text-4xl text-[#B8FFBB]">{"// Hi there!"}</h2>
+          <h1 className="text-4xl lg:text-6xl text-[#DADADA] font-bold">
+            I&apos;m Burak, a{" "}
+            <span className="underline">full-stack developer</span> and computer
+            science student.
+          </h1>
+        </div>
+        <Skills />
+      </div>
     </div>
   );
 }
---
+++ src/components/skills.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export function Skills() {
+  return (
+    <div className="w-fit drop-shadow-lg">
+      <a href="https://skillicons.dev">
+        <Image className="max-h-[84px] object-contain" src={"/skills.png"} alt="My skillset" width={1118} height={220}/>
+      </a>
+    </div>
+  );
+}
